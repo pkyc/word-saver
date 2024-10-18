aaef9d979bec937064e77b6f138b029cb596cef8
pkyc <peterkycheung@gmail.com>
2024-10-18 16:02:19 -0700

    10-18

--- api/save-item.js
+++ api/save-item.js
@@ -3,6 +3,11 @@ const { Pool } = require('pg');
 const cors = require('cors');
 
 require('dotenv').config();
+console.log('DB_HOST:', process.env.DB_HOST);
+console.log('DB_USER:', process.env.DB_USER);
+console.log('DB_PASSWORD:', process.env.DB_PASSWORD);
+console.log('DB_NAME:', process.env.DB_NAME);
+console.log('DB_PORT:', process.env.DB_PORT);
 
 const app = express();
 const pool = new Pool({
