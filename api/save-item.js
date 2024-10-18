bb1a2be212749672292b9fba157a19cb98edeef1
pkyc <peterkycheung@gmail.com>
2024-10-18 16:51:25 -0700

    10-18

--- api/save-item.js
+++ api/save-item.js
@@ -41,6 +41,12 @@ module.exports = async (req, res) => {
         }
     });
     
+    console.log('DB_HOST:', process.env.DB_HOST);
+    console.log('DB_USER:', process.env.DB_USER);
+    console.log('DB_PASSWORD:', process.env.DB_PASSWORD);
+    console.log('DB_NAME:', process.env.DB_NAME);
+    console.log('DB_PORT:', process.env.DB_PORT);
+    
     app.use(cors());
     app.use(express.json());
     app.use(express.static('public'));
