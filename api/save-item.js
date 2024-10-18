0b90dbee60d3efb3bfbd134d0efa402f267eedb4
pkyc <peterkycheung@gmail.com>
2024-10-18 16:21:43 -0700

    10-18

--- api/save-item.js
+++ api/save-item.js
@@ -8,6 +8,7 @@ console.log('DB_USER:', process.env.DB_USER);
 console.log('DB_PASSWORD:', process.env.DB_PASSWORD);
 console.log('DB_NAME:', process.env.DB_NAME);
 console.log('DB_PORT:', process.env.DB_PORT);
+console.error('Error connecting to the database:');
 
 const app = express();
 const pool = new Pool({
