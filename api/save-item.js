fb21bc83825c5ec48c58aebbc05dd44f2977443c
pkyc <peterkycheung@gmail.com>
2024-10-21 19:23:40 -0700

    10-21

--- api/save-item.js
+++ api/save-item.js
@@ -4,13 +4,6 @@ const cors = require('cors');
 
 require('dotenv').config();
 
-console.log('DB_HOST:', process.env.DB_HOST);
-console.log('DB_USER:', process.env.DB_USER);
-console.log('DB_PASSWORD:', process.env.DB_PASSWORD);
-console.log('DB_NAME:', process.env.DB_NAME);
-console.log('DB_PORT:', process.env.DB_PORT);
-console.error('Error connecting to the database:');
-
 const app = express();
 const pool = new Pool({
     user: process.env.DB_USER,
@@ -26,7 +19,6 @@ const pool = new Pool({
 app.use(cors());
 app.use(express.json());
 app.use(express.static('public'));
-
 
 module.exports = async (req, res) => {
 
