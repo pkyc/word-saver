b423ac2b2baf9eab3c6ec0426580f4feccb50bf0
pkyc <peterkycheung@gmail.com>
2024-12-10 19:16:19 -0800

    12-10

--- api/search.js
+++ api/search.js
@@ -12,7 +12,11 @@ export default async function handler(req, res) {
   }
 
   const client = new Client({
-    connectionString: process.env.NEON_CONNECTION_STRING,
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
     ssl: { rejectUnauthorized: false },
   });
 
