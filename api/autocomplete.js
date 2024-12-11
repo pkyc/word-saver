8143750c4ebf8360469255c7e7bdec1ef4cc4a09
pkyc <peterkycheung@gmail.com>
2024-12-10 19:13:40 -0800

    12-10

--- api/autocomplete.js
+++ api/autocomplete.js
@@ -11,8 +11,13 @@ export default async function handler(req, res) {
     return res.status(400).json({ error: 'Query is required' });
   }
 
+
   const client = new Client({
-    connectionString: process.env.NEON_CONNECTION_STRING,
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
     ssl: { rejectUnauthorized: false },
   });
 
